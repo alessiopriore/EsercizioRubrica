71e8b75910ed6889fc5e79ddd5ef1fd5ce5bb5b3
Alessio Priore <Alessio Priore>
2017-04-07 19:24:20 +0200

    features view

---
+++ EsRubrica/src/main/java/com/alessiopriore/view/TipoTelView.java
@@ -0,0 +1,5 @@
+package com.alessiopriore.view;
+
+public class TipoTelView {
+
+}
---
+++ EsRubrica/src/main/java/com/alessiopriore/view/UtenteView.java
@@ -0,0 +1,26 @@
+package com.alessiopriore.view;
+
+import com.alessiopriore.model.UtenteModel;
+
+public class UtenteView {
+    
+	private UtenteModel utente;
+	
+	public UtenteView(UtenteModel utente){
+		this.utente = utente;
+	}
+	
+	public UtenteModel getUtente() {
+		return utente;
+	}
+
+	public void setUtente(UtenteModel utente) {
+		this.utente = utente;
+	}
+	
+	public void printInfo(){
+		System.out.println(utente.toString());
+	}
+
+
+}
