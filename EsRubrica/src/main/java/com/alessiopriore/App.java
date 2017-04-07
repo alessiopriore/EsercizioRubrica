06814fb5b2266f7230f9865d9a26665a8905d433
Alessio Priore <Alessio Priore>
2017-04-07 18:39:27 +0200

    Inizio progetto

---
+++ EsRubrica/src/main/java/com/alessiopriore/App.java
@@ -0,0 +1,13 @@
+package com.alessiopriore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
